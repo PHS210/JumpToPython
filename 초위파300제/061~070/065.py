df8a803a10b583e4af90fd93facaa6faaee0ea64
박현서 <marios210@naver.com>
2025-09-08 21:37:45 +0900

    초위파 ~ 070

---
+++ 초위파300제/061~070/065.py
@@ -0,0 +1,5 @@
+interest = ['삼성전자', 'LG전자', 'Naver']
+# print("출력예시: \n" + interest[0] + " " + interest[2])
+print("출력예시: \n" + interest[0], interest[2])
+
+# join() 메서드 이용해서도 가능
---
+++ 초위파300제/061~070/069.py
@@ -0,0 +1,3 @@
+string = "삼성전자/LG전자/Naver"
+ls = string.split("/")
+print(ls)
